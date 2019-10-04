d8705127f82d92310f0ee1591f31a3a6657ffc67
VortexCallouts <ftvortex1@gmail.com>
2019-10-03 19:51:06 -0500

    Added Project to Github repo forgot to make repo before hand

---
+++ Documents Core Data/Model/Document+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+
+
+import Foundation
+import CoreData
+
+
+extension Document {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Document> {
+        return NSFetchRequest<Document>(entityName: "Document")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var size: Int64
+    @NSManaged public var rawModifiedDate: NSDate?
+    @NSManaged public var content: String?
+
+}
---
+++ Documents Core Data/SearchScope.swift
@@ -0,0 +1,21 @@
+
+
+import Foundation
+
+enum SearchScope: String {
+    case all
+    case name
+    case content
+    
+    static var titles: [String] {
+        get {
+            return [SearchScope.all.rawValue, SearchScope.name.rawValue, SearchScope.content.rawValue]
+        }
+    }
+    
+    static var scopes: [SearchScope] {
+        get {
+            return [SearchScope.all, SearchScope.name, SearchScope.content]
+        }
+    }
+}
